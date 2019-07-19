8a4379ae1de37c9ab282fc116582d7391873ef39
Chris Lanus <chris.lanus@isobar.com>
2019-07-19 14:46:02 -0500

    stripping down main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,65 @@
 import React from "react"
+import styled from "styled-components"
 
-import Grid from "../components/Grid"
-import Hero from "../components/Hero"
-import AboutMe from "../components/AboutMe"
-import PhotoTile from "../components/PhotoTile"
-import Footer from "../components/Footer"
+import LinkList from '../components/LinkList'
 
-const IndexPage = () => (
-    <Grid>
-        <Hero />
-        <PhotoTile />
-        <AboutMe />
-    </Grid>
+import me from "../images/me.jpg"
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Body = styled.div`
+  max-width: 80ch;
+`
+
+const ContactInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Text = styled.div`
+  font-family: "Courier New", Courier, monospace;
+`
+
+const ImageContainer = styled.div`
+    height: 5em;
+    width: 5em;
+    border-radius: 50%;
+    background: url(${me});
+    background-size: contain;
+    margin-bottom: .5em;
+`
+
+const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2em;
+  display: flex;
+  justify-content: center;
+  font-size: .6em;
+`
+
+const Main = () => (
+    <Container>
+        <Body>
+            <ContactInfo>
+                <ImageContainer />
+                <Text>Chris Lanus</Text>
+                <Text>Software Developer</Text>
+                <Text>Chicago, IL</Text>
+                <LinkList/>
+            </ContactInfo>
+        </Body>
+        <Footer>
+            <Text>Christopher Lanus 2019</Text>
+        </Footer>
+    </Container>
 )
 
-export default IndexPage
+export default Main
---
+++ src/pages/portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+`
+
+const Portfolio = () => (
+    <Container>
+        <h1>Portfolio</h1>
+        <a href="https://weather.chrislanus.com">Look at my latest project</a>
+    </Container>
+)
+
+export default Portfolio
